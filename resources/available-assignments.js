2012a39c488459d0cf1006d7ae800df6274064d6
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2017-11-15 22:44:00 +0100

    playing with AvailableAssignments

--- resources/available-assignments.js
+++ resources/available-assignments.js
@@ -1,13 +1,16 @@
 ﻿(function () {
     'use strict';
 
-    var result = fetch('https://polisens-volontarer-api.azurewebsites.net/api/AvailableAssignments');
+    var result = fetch('https://polisens-volontarer-api.azurewebsites.net/api/AvailableAssignments', {
+        method: 'GET',
+        credentials: 'include'
+    });
     result.then(function (response) {
         console.log('response', response);
         console.log('header', response.headers.get('Content-Type'));
-        return response.text();
-    }).then(function (text) {
-        console.log('got text', text);
+        return response.json();
+    }).then(function (json) {
+        console.log('got text', json);
         var output = document.querySelector('#output');
         output.innerHTML = (text);
     }).catch(function (ex) {
