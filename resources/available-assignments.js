a5b5ee94b3a9ee50130895a9b45ec12324c52062
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2017-11-15 23:35:28 +0100

    learning to fetch

--- resources/available-assignments.js
+++ resources/available-assignments.js
@@ -18,15 +18,16 @@
         console.log('header', response.headers.get('Content-Type'));
         return response.json();
     }).then(function (array) {
-
+        var items = [];
         for (var index = 0; index < array.length; index++) {
             var item = array[index];
             var date = new Date(item.date);
             var monthNumber = date.getMonth() + 1;
             var dayOfMonth = date.getDate();
             var dayOfWeekNumber = date.getDay();
-            var dayOfWeekName = '';
             var weekNumber = date.getWeekNumber();
+
+            var dayOfWeekName = '';
             switch (dayOfWeekNumber) {
                 case 1:
                     dayOfWeekName = 'Måndag';
@@ -50,11 +51,66 @@
                     dayOfWeekName = 'Söndag';
                     break;
             }
-            console.log(item.date, dayOfWeekName, weekNumber, dayOfMonth, monthNumber);
+
+            var monthName = '';
+            switch (monthNumber) {
+                case 1:
+                    monthName = 'Januari';
+                    break;
+                case 1:
+                    monthName = 'Januari';
+                    break;
+                case 2:
+                    monthName = 'Februari';
+                    break;
+                case 3:
+                    monthName = 'Mars';
+                    break;
+                case 4:
+                    monthName = 'April';
+                    break;
+                case 5:
+                    monthName = 'Maj';
+                    break;
+                case 6:
+                    monthName = 'Juni';
+                    break;
+                case 7:
+                    monthName = 'Juli';
+                    break;
+                case 8:
+                    monthName = 'Augusti';
+                    break;
+                case 9:
+                    monthName = 'September';
+                    break;
+                case 10:
+                    monthName = 'Oktober';
+                    break;
+                case 11:
+                    monthName = 'November';
+                    break;
+                case 12:
+                    monthName = 'December';
+                    break;
+            }
+
+            items.push({
+                monthName: monthName,
+                weekNumber: weekNumber,
+                dayOfWeekNumber: dayOfWeekNumber,
+                dayOfWeekName: dayOfWeekName,
+                assignmentName: item.name,
+                category: item.category,
+                area: item.area
+            });
         }
-        console.log('got text', json);
+        console.log('items', items);
         var output = document.querySelector('#output');
         output.innerHTML = (json);
+        return items;
+    }).then(function(item) {
+        console.log(item);
     }).catch(function (ex) {
         console.log('failed', ex);
     });
