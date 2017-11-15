e37fe8b74c8805e9fcd67532fda8c4e865391b21
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2017-11-15 23:14:30 +0100

    getting date information

--- resources/available-assignments.js
+++ resources/available-assignments.js
@@ -1,4 +1,12 @@
-﻿(function () {
+﻿Date.prototype.getWeekNumber = function(){
+    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+  };
+
+(function () {
     'use strict';
 
     var result = fetch('https://polisens-volontarer-api.azurewebsites.net/api/AvailableAssignments', {
@@ -9,10 +17,44 @@
         console.log('response', response);
         console.log('header', response.headers.get('Content-Type'));
         return response.json();
-    }).then(function (json) {
+    }).then(function (array) {
+
+        for (var index = 0; index < array.length; index++) {
+            var item = array[index];
+            var date = new Date(item.date);
+            var monthNumber = date.getMonth() + 1;
+            var dayOfMonth = date.getDate();
+            var dayOfWeekNumber = date.getDay();
+            var dayOfWeekName = '';
+            var weekNumber = date.getWeekNumber();
+            switch (dayOfWeekNumber) {
+                case 1:
+                    dayOfWeekName = 'Måndag';
+                    break;
+                case 2:
+                    dayOfWeekName = 'Tisdag';
+                    break;
+                case 3:
+                    dayOfWeekName = 'Onsdag';
+                    break;
+                case 4:
+                    dayOfWeekName = 'Torsdag';
+                    break;
+                case 5:
+                    dayOfWeekName = 'Fredag';
+                    break;
+                case 6:
+                    dayOfWeekName = 'Lördag';
+                    break;
+                case 0:
+                    dayOfWeekName = 'Söndag';
+                    break;
+            }
+            console.log(item.date, dayOfWeekName, weekNumber, monthNumber);
+        }
         console.log('got text', json);
         var output = document.querySelector('#output');
-        output.innerHTML = (text);
+        output.innerHTML = (json);
     }).catch(function (ex) {
         console.log('failed', ex);
     });
