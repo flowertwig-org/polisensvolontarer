8dfce19c6b045fae257a794d576017f1ba3f5723
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-01-02 21:24:45 +0100

    started on google calendar link

--- resources/assignment.js
+++ resources/assignment.js
@@ -36,9 +36,14 @@
             var assignmentDescription = templateAssignment.content.querySelector(".assignment-description");
             assignmentDescription.innerHTML = assignment.description;
             var assignmentAdded = templateAssignment.content.querySelector(".assignment-added");
-            assignmentAdded.textContent = assignment.currentNumberOfPeople + ' st';
+            assignmentAdded.textContent = assignment.currentNumberOfPeople;
             var assignmentRequested = templateAssignment.content.querySelector(".assignment-requested");
-            assignmentRequested.textContent = assignment.wantedNumberOfPeople + ' st';
+            assignmentRequested.textContent = assignment.wantedNumberOfPeople;
+
+            var assignmentCalendar = templateAssignment.content.querySelector(".assignment-calendar-google");
+            assignmentCalendar.href = "https://www.google.com/calendar/render?action=TEMPLATE&text=" + encodeURI(assignment.name) + "&dates=" + encodeURI(assignment.date) + "T224000Z/" + encodeURI(assignment.date) + "T221500Z&details=For+details,+link+here:+" + encodeURI(location.href) + "&location=&sf=true&output=xml"
+
+            // https://www.google.com/calendar/render?action=TEMPLATE&text=Your+Event+Name&dates=20140127T224000Z/20140320T221500Z&details=For+details,+link+here:+http://www.example.com&location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022&sf=true&output=xml
 
             var cloneAssignment = document.importNode(templateAssignment.content, true);
             main.appendChild(cloneAssignment);
