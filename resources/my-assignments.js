1f9be6ea782bd1c6440ba1c34ad95c386c58966c
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-01-07 22:34:03 +0100

    my assignments

---
+++ resources/my-assignments.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    var result = fetch('https://polisens-volontarer-api.azurewebsites.net/api/MyAssignments', {
+        method: 'GET',
+        credentials: 'include',
+        mode: 'cors'
+    });
+    result.then(function (response) {
+        if (response.ok) {
+            return response.json();
+            // TODO: Re enable this part when code is working as it should again
+        } else {
+            window.location.assign('/login/');
+        }
+    }).then(function (myAssignments) {
+        if (!myAssignments || !('confirms' in myAssignments)) {
+            window.location.assign('/login/');
+            return myAssignments;
+        }
+
+        var nOfConfirms = myAssignments.confirms.length;
+        document.querySelector('#my-assignments-count').textContent = nOfConfirms;
+
+        if (nOfConfirms > 0) {
+            if ('content' in document.createElement('template')) {
+                var main = document.querySelector("#my-assignments-confirms");
+                var templateAssignment = document.querySelector('#template-my-assignment');
+
+                for (let index = 0; index < nOfConfirms; index++) {
+                    const assignment = myAssignments.confirms[index];
+                    
+                    var assignmentName = templateAssignment.content.querySelector(".assignment-name");
+                    assignmentName.textContent = assignment.name;
+    
+                    assignmentName.href = "/restricted/assignment?key=" + assignment.id;
+                    var assignmentWhen = templateAssignment.content.querySelector(".assignment-when");
+                    assignmentWhen.textContent = assignment.date;
+                    var assignmentType = templateAssignment.content.querySelector(".assignment-type");
+                    assignmentType.textContent = assignment.category;
+    
+                    var cloneAssignment = document.importNode(templateAssignment.content, true);
+                    main.appendChild(cloneAssignment);
+                }
+
+            } else {
+                // TODO: Show warning message to user that it requires template support
+            }
+        }
+
+
+    }).catch(function (ex) {
+        console.log(ex);
+    });
+})();
