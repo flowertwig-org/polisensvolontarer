b591f325c8fb1fe1b3262c2b3ee5e7b05020e07f
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-08-19 20:40:08 +0200

    Första testa för att visa varningar vid användning av kända lösenord

---
+++ resources/warnings.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    var keyValuePairs = location.search.substr(1).split('&');
+    for (let index = 0; index < keyValuePairs.length; index++) {
+        const pair = keyValuePairs[index].split('=');
+        const key = pair[0];
+        const value = pair[1];
+
+        switch (key) {
+            case 'warning':
+                var warningElement = document.createElement('div');
+                warningElement.className = 'warning';
+                switch (value) {
+                    case "1":
+                        warningElement.innerText = 'Du använder ett vanligt använt lösenord och bör därför byta.';
+                        break;
+                    }
+                document.querySelector('main').insertAdjacentElement('afterbegin', warningElement);
+                break;
+        }
+    }
+})();
