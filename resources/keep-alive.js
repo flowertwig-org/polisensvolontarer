0c8f45b76828e0cc49b2a83ece2f3204f352ef44
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-11-27 21:24:09 +0100

    påbörjat bättre logik för när tredjepartskakor inte tillåts

--- resources/keep-alive.js
+++ resources/keep-alive.js
@@ -17,18 +17,37 @@
             if (response.ok) {
                 return response.json();
             }
-        }).then(function(isAlive) {
-            if (!isAlive) {
+        }).then(function(resultCode) {
+            var warning = '';
+            if (resultCode == 0) {
+                warning = 'warning=3';
+            }else if (resultCode < 0) {
+                warning = 'warning=4';
+            }
+
+            if (resultCode <= 0) {
                 // If we are not alive anymore, return to login page
                 switch (location.pathname) {
                     case '/restricted/assignment/':
-                        window.location.assign('/?page=assignment&' + window.location.search.substr(1));
+                        if (warning.length) {
+                            window.location.assign('/?' + warning + '&page=assignment&' + window.location.search.substr(1));
+                        }else {
+                            window.location.assign('/?page=assignment&' + window.location.search.substr(1));
+                        }
                         break;
                     case '/restricted/available-assignments/':
-                        window.location.assign('/?page=available-assignments');
+                        if (warning.length) {
+                            window.location.assign('/?page=available-assignments?' + warning);
+                        }else {
+                            window.location.assign('/?page=available-assignments');
+                        }
                         break;
                     default:
-                        window.location.assign('/');
+                        if (warning.length) {
+                            window.location.assign('/?' + warning);
+                        }else {
+                            window.location.assign('/');
+                        }
                         break;
                 }
             }
--- resources/warnings.js
+++ resources/warnings.js
@@ -15,7 +15,16 @@
                     case "1":
                         warningElement.innerText = 'Du använder ett vanligt använt lösenord och bör därför byta. Just nu kan du bara göra detta på: http://volontar.polisen.se';
                         break;
-                    }
+                    case "2":
+                        warningElement.innerText = 'Kunde inte logga in dig. Kontrollera att du angett rätt användarnamn och lösenord.';
+                        break;
+                    case "3":
+                        warningElement.innerText = 'Du har antingen blivit utloggad pga inaktivitet eller så är din webbläsare inställd att inte tillåta tredjeparts kakor.';
+                        break;
+                    case "4":
+                        warningElement.innerText = 'Okänt tekniskt fel inträffade, vänligen försök igen lite senare.';
+                        break;
+                }
                 document.querySelector('main').insertAdjacentElement('afterbegin', warningElement);
                 break;
         }
