3b0d6297b18838b70020a73001512b6c5ab0c5a0
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-12-21 11:35:33 +0100

    remember settings value

--- resources/service-worker-installer.js
+++ resources/service-worker-installer.js
@@ -7,6 +7,10 @@
     var cbServiceWorker = document.querySelector('#serviceworker');
     if (cbServiceWorker) {
       cbServiceWorker.disabled = false;
+      // Remember setting
+      if (sessionStorage.getItem('settings-serviceworker')) {
+        cbServiceWorker.checked = false;
+      }
 
       cbServiceWorker.addEventListener('change', function (event) {
         cbServiceWorker.disabled = true;
@@ -14,10 +18,12 @@
           navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            sessionStorage.setItem('settings-serviceworker', '1');
             cbServiceWorker.disabled = false;
           }, function (err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
+            sessionStorage.removeItem('settings-serviceworker');
             cbServiceWorker.disabled = false;
           });
         } else {
