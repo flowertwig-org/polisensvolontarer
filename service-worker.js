7d9bee2e5d9413ce897984ba9ffe84caf6f7784a
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2018-12-24 11:56:05 +0100

    test offline

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 
-var CACHE_NAME = 'volontar-cache-v1';
+var CACHE_APP_NAME = 'volontar-cache-v1';
+var CACHE_SERVICE_NAME = 'volontar-service-cache-v1';
 var urlsToCache = [
     // Pages
     '/',
@@ -34,7 +35,7 @@ var urlsToCache = [
 self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        caches.open(CACHE_APP_NAME)
             .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
@@ -47,14 +48,33 @@ self.addEventListener('fetch', function (event) {
     //     data: 'fetch'
     // }));
 
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+    const url = new URL(event.request.url);
+    //url.search = '';
+
+      // Create promises for both the network response,
+      // and a copy of the response that can be used in the cache.
+      const fetchResponseP = fetch(url);
+      const fetchResponseCloneP = fetchResponseP.then(r => r.clone());
+
+      // event.waitUntil() ensures that the service worker is kept alive
+      // long enough to complete the cache update.
+      event.waitUntil(async function() {
+        const cache = await caches.open(CACHE_SERVICE_NAME);
+        await cache.put(url, await fetchResponseCloneP);
+      }());
+
+      //return (await caches.match(url)) || fetchResponseP;
+      // Prefer the fetch response, falling back to the cached response.
+      return fetchResponseP || (await caches.match(url));
+
+    // event.respondWith(
+    //     caches.match(event.request)
+    //         .then(function (response) {
+    //             // Cache hit - return response
+    //             if (response) {
+    //                 return response;
+    //             }
+    //             return fetch(event.request);
+    //         })
+    // );
 });    
